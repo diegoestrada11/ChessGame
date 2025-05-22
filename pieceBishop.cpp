a04917c2c995ae6cc8c5f195011686dc8e2030ba
natalianavhur <nnavhur@gmail.com>
2025-05-22 08:06:08 -0600

    added piece test files

---
+++ pieceBishop.cpp
@@ -0,0 +1,1 @@
+#include "pieceBishop.h"
--- testBoard.cpp
+++ testBoard.cpp
@@ -16,15 +16,35 @@
 
 
 
-
  /********************************************************
-  *    e5c6
+  *   a2a3
   * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
   * |                     |       |                     |
   * 8                     8       8                     8
   * 7                     7       7                     7
-  * 6       .             6       6       n             6
-  * 5          (n)        5       5           .         5
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4                     4  -->  4                     4
+  * 3   .                 3       3   p                 3
+  * 2  (p)                2       2   .                 2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_pawnSimple()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *     a6b7r
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7     R               7       7    (p)              7
+  * 6  (p)                6       6   .                 6
+  * 5                     5       5                     5
   * 4                     4  -->  4                     4
   * 3                     3       3                     3
   * 2                     2       2                     2
@@ -32,88 +52,301 @@
   * |                     |       |                     |
   * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
   ********************************************************/
-void TestBoard::move_knightMove()
-{  // SETUP
-   Move e5c6;
-   e5c6.source.colRow = 0x44;
-   e5c6.dest.colRow = 0x25;
-   e5c6.capture = SPACE;
-   e5c6.promote = SPACE;
-   e5c6.isWhite = true;
-   e5c6.moveType = Move::MOVE;
-   Board board(nullptr, true /*noreset*/);
-   board.numMoves = 17;
-   board.board[4][4] = new PieceSpy(4, 4, true  /*isWhite*/, KNIGHT);
-   board.board[2][5] = new PieceSpy(2, 5, false /*isWhite*/, SPACE);
-   board.board[4][4]->nMoves = 17;
-   PieceSpy::reset();
+void TestBoard::move_pawnCapture()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
 
-   // EXERCISE
-   board.move(e5c6);
 
-   // VERIFY
-   assertUnit(18 == board.numMoves);
-   assertUnit(SPACE == (board.board[4][4])->getType());
-   assertUnit(KNIGHT == (board.board[2][5])->getType());
-   assertUnit(PieceSpy::numConstruct == 0);
-   assertUnit(PieceSpy::numCopy == 0);
-   assertUnit(PieceSpy::numDelete == 0);
-   assertUnit(PieceSpy::numAssign == 0);
-   assertUnit(PieceSpy::numMove == 0);
+ /********************************************************
+  *    e2e4
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4           .         4  -->  4          (p)        4
+  * 3                     3       3                     3
+  * 2          (p)        2       2           .         2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_pawnDouble()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
 
-   // TEARDOWN
-   delete board.board[2][5];
-   delete board.board[4][4];
-   board.board[2][5] = board.board[4][4] = nullptr;
+
+ /********************************************************
+ *     a5b6E
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6     .               6       6     p               6
+  * 5  (p)P               5       5   . .               5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_pawnEnpassant()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *    a7a8Q 
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8   .                 8       8  (r)                8
+  * 7  (p)                7       7   .                 7
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_pawnPromotion()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *    e5a5
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5   .      (r)        5       5  (r)      .         5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_rookSlide()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *    e5a5b
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5   B      (r)        5       5  (r)      .         5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_rookAttack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *    e5g3
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5          (b)        5       5           .         5
+  * 4                     4  -->  4                     4
+  * 3               .     3       3               b     3
+  * 2                     2       2                     2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_bishopSlide()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *    e5g3q
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5          (b)        5       5           .         5
+  * 4                     4  -->  4                     4
+  * 3               Q     3       3               b     3
+  * 2                     2       2                     2
+  * 1                     1       1                     1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_bishopAttack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
 }
 
 /********************************************************
-*    e5c6r
+ *    e5g3
+ * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+ * |                     |       |                     |
+ * 8                     8       8                     8
+ * 7                     7       7                     7
+ * 6                     6       6                     6
+ * 5          (q)        5       5                     5
+ * 4                     4  -->  4           .         4
+ * 3                .    3       3               q     3
+ * 2                     2       2                     2
+ * 1                     1       1                     1
+ * |                     |       |                     |
+ * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+ ********************************************************/
+void TestBoard::move_queenSlide()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+/********************************************************
+*    e5a5b
 * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
 * |                     |       |                     |
 * 8                     8       8                     8
-* 7       R             7       7                     7
-* 6                     6       6       n             6
-* 5          (n)        5       5           .         5
+* 7                     7       7                     7
+* 6                     6       6                     6
+* 5   B      (q)        5       5  (q)      .         5
 * 4                     4  -->  4                     4
 * 3                     3       3                     3
 * 2                     2       2                     2
 * 1                     1       1                     1
 * |                     |       |                     |
 * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
- ********************************************************/
-void TestBoard::move_knightAttack()
-{  // SETUP
-   Move e5c6r;
-   e5c6r.source.colRow = 0x44;
-   e5c6r.dest.colRow = 0x25;
-   e5c6r.capture = ROOK;
-   e5c6r.promote = SPACE;
-   e5c6r.isWhite = true;
-   e5c6r.moveType = Move::MOVE;
-   Board board(nullptr, true /*noreset*/);
-   board.numMoves = 17;
-   board.board[4][4] = new PieceSpy(4, 4, true  /*isWhite*/, KNIGHT);
-   board.board[2][5] = new PieceSpy(2, 5, false /*isWhite*/, ROOK);
-   board.board[4][4]->nMoves = 17;
-   PieceSpy::reset();
-
-   // EXERCISE
-   board.move(e5c6r);
-
-   // VERIFY
-   assertUnit(18 == board.numMoves);
-   assertUnit(SPACE == (board.board[4][4])->getType());
-   assertUnit(KNIGHT == (board.board[2][5])->getType());
-   assertUnit(PieceSpy::numConstruct == 0);
-   assertUnit(PieceSpy::numCopy == 0);
-   assertUnit(PieceSpy::numDelete == 0);
-   assertUnit(PieceSpy::numAssign == 0);
-   assertUnit(PieceSpy::numMove == 0);
-
-   // TEARDOWN
-   delete board.board[2][5];
-   delete board.board[4][4];
-   board.board[2][5] = board.board[4][4] = nullptr;
+********************************************************/
+void TestBoard::move_queenAttack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
 }
 
+
+ /********************************************************
+  *  e1f1
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1          (k).       1       1           . k       1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_kingMove()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+ /********************************************************
+  *    e1f1r
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1          (k)R       1       1           . k       1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_kingAttack()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+ /********************************************************
+  *    e1g1c
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1          (k). . r   1       1           . r k .   1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_kingShortCastle()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+
+ /********************************************************
+  *    e1c1C 
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  * |                     |       |                     |
+  * 8                     8       8                     8
+  * 7                     7       7                     7
+  * 6                     6       6                     6
+  * 5                     5       5                     5
+  * 4                     4  -->  4                     4
+  * 3                     3       3                     3
+  * 2                     2       2                     2
+  * 1   r . . .(k)        1       1   . . k r .         1
+  * |                     |       |                     |
+  * +---a-b-c-d-e-f-g-h---+       +---a-b-c-d-e-f-g-h---+
+  ********************************************************/
+void TestBoard::move_kingLongCastle()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
+
+
+
+
+/******************************************************** 
+ *        +---a-b-c-d-e-f-g-h---+
+ *        |                     |
+ *        8   R N B Q K B N R   8
+ *        7   P P P P P P P P   7
+ *        6                     6
+ *        5                     5
+ *   -->  4                     4
+ *        3                     3
+ *        2   p p p p p p p p   2
+ *        1   r n b q k b n r   1
+ *        |                     |
+ *        +---a-b-c-d-e-f-g-h---+
+ ********************************************************/
+void TestBoard::construct_default()
+{
+   assertUnit(NOT_YET_IMPLEMENTED);
+}
